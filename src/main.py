b996e9196131fae246a2cc478fd424f4d00bcb73
Abhiram Kondapalli <abhiramkondapalli@gods-MacBook.local>
2026-09-06 18:17:50 -0400

    Add random lottery function

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
-print("ParkFair is running!")
+import random
+
+def random_lottery(applicants, parking_spots):
+    selected_students = random.sample(applicants, parking_spots)
+    return selected_students
+
+
+applicants = [1, 2, 3, 4, 5]
+parking_spots = 3
+
+winners = random_lottery(applicants, parking_spots)
+
+print("Selected students:", winners)
